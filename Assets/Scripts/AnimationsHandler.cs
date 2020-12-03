090e5bbbc716fc84fcb958fa282533552db6f764
Borna Zbodulja <borna.zbodulja@gmail.com>
2020-12-04 00:20:56 +0100

    created script for handling animations

---
+++ Assets/Scripts/AnimationsHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationsHandler : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButton("Jump") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            animator.SetTrigger("jump");
+        }
+        else
+        {
+            float forward = Input.GetAxis("Vertical");
+            if (forward < 0 && Input.GetKey(KeyCode.LeftShift))
+            {
+                forward = -1f;
+            }
+            else if (forward > 0 && Input.GetKey(KeyCode.LeftShift))
+            {
+                forward = 1f;
+            }
+            float turn = Input.GetAxis("Horizontal");
+
+            animator.SetFloat("forward", forward);
+            animator.SetFloat("turn", turn);
+        }
+    }
+}
